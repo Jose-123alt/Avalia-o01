5d12cb98ffc77bd7cd00abacda59a8c76efab6a0
Jose-123alt <josevaqueiroh@gmail.com>
2025-12-18 10:13:03 -0300

    Add ConversaoTemperatura class for temperature conversion

---
+++ Atividade02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConversaoTemperatura {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Entrada de dados
+        System.out.print("Digite a temperatura em Celsius: ");
+        double celsius = sc.nextDouble();
+
+        // Conversões
